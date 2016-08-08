4e7781c78748b07339c78a182627d2edd2fea7bf
vilyever <250450035@163.com>
2016-08-08 10:35:50 +0800

    Initial Commit

---
+++ objcBasic/objcBasic.h
@@ -0,0 +1,19 @@
+//
+//  objcBasic.h
+//  objcBasic
+//
+//  Created by FTET on 16/8/8.
+//  Copyright © 2016年 vilyever. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for objcBasic.
+FOUNDATION_EXPORT double objcBasicVersionNumber;
+
+//! Project version string for objcBasic.
+FOUNDATION_EXPORT const unsigned char objcBasicVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <objcBasic/PublicHeader.h>
+
+
